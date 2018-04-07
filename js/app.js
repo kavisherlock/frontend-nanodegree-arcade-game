4ae36c6217c3edb837431a9b2f56899b55769431
Kavish <kavishrmunjal@gmail.com>
2018-04-07 16:37:31 -0500

    Completed assignment

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
+var ROW_HEIGHT = 83;
+var COL_WIDTH = 101;
+var WATER_OFFSET = 20;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row, velocity) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = 0;
+    this.y = row * ROW_HEIGHT - WATER_OFFSET;
+    this.vx = velocity;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +22,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.vx * dt;
+    if (this.x > 5 * COL_WIDTH) {
+      this.x = -1 * COL_WIDTH;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +33,59 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Enemy.prototype.checkCollision = function(playerX, playerY) {
+  if (this.y != playerY) {
+    return false;
+  }
+  if (this.x > playerX - COL_WIDTH + 30 && this.x < playerX + COL_WIDTH - 30) {
+    return true;
+  }
 
+  return false;
+};
+
+
+var Player = function() {
+    this.sprite = 'images/char-pink-girl.png';
+
+    this.x = 2 * COL_WIDTH;
+    this.y = 5 * ROW_HEIGHT - WATER_OFFSET;
+};
+
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+      case 'left':
+        this.x = Math.max(0, this.x - COL_WIDTH);
+        break;
+      case 'right':
+        this.x = Math.min(4 * COL_WIDTH, this.x + COL_WIDTH);
+        break;
+      case 'up':
+        this.y = Math.max(-1 * WATER_OFFSET, this.y - ROW_HEIGHT);
+        break;
+      case 'down':
+        this.y = Math.min(5 * ROW_HEIGHT - WATER_OFFSET, this.y + ROW_HEIGHT);
+        break;
+    }
+};
+
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.reset = function(dt) {
+  this.x = 2 * COL_WIDTH;
+  this.y = 5 * ROW_HEIGHT - WATER_OFFSET;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy(1, 200), new Enemy(2, 300), new Enemy(3, 100)];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -79,7 +79,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -94,6 +94,14 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+    }
+
+    function checkCollisions() {
+        allEnemies.forEach(function(enemy) {
+            if (enemy.checkCollision(player.x, player.y)) {
+              player.reset();
+            }
+        });
     }
 
     /* This function initially draws the "game level", it will then call
@@ -117,7 +125,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
@@ -161,7 +169,7 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+      //noop
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -173,7 +181,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-pink-girl.png'
     ]);
     Resources.onReady(init);
 
